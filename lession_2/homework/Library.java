bce8d0715227066957cf7df7247d9e605a8351c9
Vinh Vu Ngoc <vungocvinh95@gmail.com>
2021-10-09 19:20:12 +0700

    done homework lession 2

--- lession_2/homework/Library.java
+++ lession_2/homework/Library.java
@@ -2,12 +2,13 @@ package homework;
 
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 
 public class Library {
     private String libraryName;
-    private Book[] bookList;
+    private Book[] bookList = new Book[100];
     private static int numberOfBooks;
     public static final int MAX_NUMBER_OF_BOOKS = 100;
     
@@ -18,42 +19,125 @@ public class Library {
     public void addNewBook() {
         Scanner input = new Scanner(System.in);
         System.out.println("2. Add new book");
-        System.out.println("--------------------");
-        System.out.println("Enter book's infomation: ");
-        System.out.println("(1) Book ID: ");
-        int id = input.nextInt();
-        System.out.println("(2) Book Title: ");
-        String title = input.nextLine();
-        System.out.println("(3) Amount: ");
-        int amount = input.nextInt();
-        Book book = new Book(id, title, amount);
-        int len = this.bookList.length;
-        for(int i = 0; i < len; i++) {
-            if(this.bookList[i].getbookID() == id) {
-                System.out.println("Book id is exits");
-                break;
+        System.out.println("-----------------------");
+        if (numberOfBooks >= MAX_NUMBER_OF_BOOKS) {
+            System.out.println("Library is fully");
+        } else {
+            System.out.println("Enter book's infomation: ");
+            System.out.println("(1) Book ID: ");
+            
+            int id = input.nextInt();
+            System.out.println("(2) Book Title: ");
+            
+            String title = input.nextLine();
+            System.out.println("(3) Amount: ");
+            
+            int amount = input.nextInt();
+            Book book = new Book(id, title, amount);
+
+            boolean result = Arrays.stream(bookList).anyMatch(book::equals);
+            if(result) {
+                System.out.println("Book is already exits");
+                System.exit(0);
+            } else {
+                for(int i = 0; i < 100; i++) {
+                    if(bookList[i] == null) {
+                        bookList[i] = book;
+                        bookList[i].showBookInfo();
+                        System.exit(0);
+                    }
+                }
             }
-        }
-        if(len <= 100) {
-            Book[] listBook = Arrays.copyOf(this.bookList, len + 1);
-            listBook[listBook.length - 1] = book;
-            this.bookList = listBook;
         }
     }
 
     public boolean findBook(int bookID) {
-
+        System.out.println("3. Find book");
+        System.out.println("-----------------------");
+        System.out.println("Enter book's ID: " + bookID);
+        for(int i = 0; i < MAX_NUMBER_OF_BOOKS; i++) {
+            if(bookList[i].getbookID() == bookID) {
+                bookList[i].showBookInfo();
+                return true;
+            }
+            else {
+                System.out.println("The book is not found");
+                System.exit(0);
+                return false;
+            }
+        }
+        return false;
     }
 
     public void borrowBook(int bookID) {
-
+        System.out.println("4. Borrow a book");
+        System.out.println("-----------------------");
+        System.out.println("Enter book's ID: " + bookID);
+        for(int i = 0; i < MAX_NUMBER_OF_BOOKS; i++) {
+            if(bookList[i].getbookID() == bookID && bookList[i].getamount() > 0) {
+                bookList[i].showBookInfo();
+                System.out.println("The book is borrowed successfully!");
+                System.out.println("\n");
+                bookList[i].setamount(bookList[i].getamount() - 1);
+                bookList[i].showBookInfo();
+            } else {
+                System.out.println("The book is can not borrowed!");
+            }
+        }
     }
 
     public void returnBook(int bookID) {
-
+        System.out.println("5. Return a book");
+        System.out.println("-----------------------");
+        System.out.println("Enter book's ID: " + bookID);
+        for(int i = 0; i < MAX_NUMBER_OF_BOOKS; i++) {
+            if(bookList[i].getbookID() == bookID) {
+                System.out.println("The book is return successfully!");
+                bookList[i].setamount(bookList[i].getamount() + 1);
+                bookList[i].showBookInfo();
+            } else {
+                System.out.println("The book is can not returned!");
+            }
+        }
     }
 
     public void showLibraryInfo() {
+        System.out.println("Name: " + libraryName);
+        System.out.println("Number of books: " + numberOfBooks);
+        for(int i = 0; i < MAX_NUMBER_OF_BOOKS; i++) {
+            bookList[i].showBookInfo();
+        }
+    }
+
+    public boolean findBook(String keyword) {
+        System.out.println("3. Find book");
+        System.out.println("-----------------------");
+        System.out.println("Enter book's title: " + keyword);
+        for(int i = 0; i < MAX_NUMBER_OF_BOOKS; i++) {
+            if(bookList[i].getbookTitle() == keyword) {
+                bookList[i].showBookInfo();
+                return true;
+            }
+            else {
+                System.out.println("The book is not found");
+                System.exit(0);
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public void getALuckyBook() {
+        Random randomNumber = new Random();
+        int rand = randomNumber.nextInt(100);
+        for(int i = 0; i < MAX_NUMBER_OF_BOOKS; i++) {
+            if(bookList[i].getbookID() == rand) {
+                System.out.println("A lucky book is: ");
+                bookList[i].showBookInfo();
+            } else {
+                System.out.println("This book is not found, please choose again");
+            }
+        }
 
     }
 }
--- lession_2/homework/Program.java
+++ lession_2/homework/Program.java
@@ -1,20 +1,58 @@
 package homework;
 
+import java.util.Scanner;
+
 
 public class Program {
+    static Library library = new Library("Thu vien to dan pho 7, phuong Bach Khoa");
     public static void main(String args[]) {
-        
-    }
-
-    private static int option() {
-
+        showMenu();
     }
 
     public static void showMenu() {
-
+        System.out.println("-----------Main menu---------");
+        System.out.println("1. Show library information");
+        System.out.println("2. Add new book");
+        System.out.println("3. Find book");
+        System.out.println("4. Borrow book");
+        System.out.println("5. Return a book");
+        System.out.println("6. Exit");
+        System.out.println("-----------------------------");
+        System.out.print("Enter menu ID (1-6): ");
+        askOption();
     }
 
     public static void askOption() {
-
+        Scanner input = new Scanner(System.in);
+        int option = input.nextInt();
+        
+        switch(option) {
+        case 1:
+            library.showLibraryInfo();
+            break;
+        case 2:
+            library.addNewBook();
+            break;
+        case 3:
+            System.out.println("Fill in id of book which you want to find: ");
+            int id1 = input.nextInt();
+            library.findBook(id1);
+            break;
+        case 4:
+            System.out.println("Fill in id of book which you want to borrow: ");
+            int id2 = input.nextInt();
+            library.borrowBook(id2);
+            break;
+        case 5:
+            System.out.println("Fill in id of book which you want to return: ");
+            int id3 = input.nextInt();
+            library.returnBook(id3);
+            break;
+        case 6:
+            System.out.println("Goodbye!");
+            System.exit(0);
+            break;
+        }
+        
     }
 }
